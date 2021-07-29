0b3f07449c17075abf688f7a8185d407ea02b9af
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-07-29 14:24:34 +0300

    CheckBox's last option can be disabled
    Last checkbox can be disabled if any other checkbox is selected. The last checkbox is not disabled if every checkbox seen is empty.

--- src/components/typeComponents/CheckBox.js
+++ src/components/typeComponents/CheckBox.js
@@ -12,39 +12,48 @@ import Box from '@material-ui/core/Box'
 const CheckBox = ({ question }) => {
 	const { currentPage, handleInputChange, formData } = useForm()
 	const currentValues = getAnswerByID(currentPage, question.id, formData)
+	const checkedValues = currentValues.value.filter(
+		(values) => values.isChecked
+	)
 
 	const handleCheckBox = (event, id) => {
+		// Check if last box is disabled
+		// If it is, make sure to update its state as NOT checked
+		// If it is not disabled, update state as normally would
+		let disabledBox = currentValues.value.find((answer) => answer.id === 6)
+		if (
+			isDisabled(6) ||
+			checkedValues.find((checkbox) => checkbox.id !== id)
+		) {
+			disabledBox.isChecked = false
+		}
+
 		const checkedBox = {
 			id,
 			text: event.target.name,
 			isChecked: event.target.checked
 		}
+
 		const updatedValues = currentValues.value.map((answer) => {
-			return answer.id === id ? checkedBox : answer
+			// Update disabled checkbox only if the target box is not the disabled box
+			if (answer.id === 6 && id !== 6) {
+				return disabledBox
+				// Update the targeted box
+			} else if (answer.id === id) {
+				return checkedBox
+				// Update non-target and not disabled box
+			} else {
+				return answer
+			}
 		})
+		// If check box is disabled and other box is targeted, two checkboxes are updated in the state
 		handleInputChange(question.id, updatedValues)
 	}
 
 	const isDisabled = (id) => {
 		// Only the last option needs to be disabled
 		// Last option is disabled if any other option is checked
-		const checkedValues = currentValues.value.filter(
-			(values) => values.isChecked
-		)
 		if (checkedValues.find((checkbox) => checkbox.id !== id)) {
-			// Uncheck "En suunnittele" checkbox
-			if (checkedValues.find((checkbox) => checkbox.id === id)) {
-				const updatedValues = currentValues.value.map((answer) => {
-					return answer.id === id
-						? {
-								...answer,
-								isChecked: false
-						  }
-						: answer
-				})
-				// Dispatch to update isChecked for last option
-				handleInputChange(question.id, updatedValues)
-			}
 			// Disable "En suunnittele" checkbox
 			return true
 		}
